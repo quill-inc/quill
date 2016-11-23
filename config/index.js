04f2ff4229d36cd3aa1a85a3a1d64ae067cc8198
Eric Martindale <eric@ericmartindale.com>
2016-11-23 15:25:41 -0800

    Add launch page.

--- config/index.js
+++ config/index.js
@@ -2,17 +2,21 @@ module.exports = {
   service: {
     name: 'Quill', // change this!
     icon: 'pencil', // pick from: http://semantic-ui.com/elements/icon.html
-    pitch: 'Create a World Together.',
+    pitch: 'Create Worlds Together.',
     synopsis: 'Write better, together.', // and this...
     mission: 'Design your own universe with Quill\'s collaboration engine.', // also
     description: 'RolePlayGateway helps you collaborate with fellow writers to create your own world. We\'ve created a set of roleplaying tools to help you organize your worlds and write with your fellow authors.',
-    about: 'Quill is designed <strong>with <i class="heart icon"></i>love</strong> by a caring community of creators from all around the world.  We believe that everyone has the right to write, re-write, and tell amazing stories through collaboratively-designed and digitally-organized storytelling experiences.',
+    about: 'Quill is designed <strong>with <i class="heart icon"></i>love</strong> by a caring community of creators from all around the world.  Made with Maki.',
     copyright: 'Quill enforces no copyright over any content published by its users, and serves only as a conduit through which they share their story.',
+    masthead: 'https://www.roleplaygateway.com/images/sea-of-stars.jpg',
     cta: {
+      component: '<maki-waiting-list></maki-waiting-list>'
+    },
+    /*cta: {
       link: '/universes',
       text: 'View All <span><i class="globe icon"></i></span>Worlds',
       icon: 'globe'
-    },
+    },*/
     points: [
       {
         header: 'e',
