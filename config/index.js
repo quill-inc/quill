04566adb0eca0a50405e979b277846e0de5595ec
Eric Martindale <eric@ericmartindale.com>
2016-11-26 18:55:55 -0800

    Update copy to match new brand

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     synopsis: 'Quill is a next-generation writing tool.', // and this...
     mission: '<span class="brand">Quill</span> is a next-generation writing tool<br /> carefully hand-crafted for your needs.', // also
     description: 'Quill is a next-generation writing tool that works anywhere and everywhere.  Organize your writing with our intuitive interface, connecting concepts for your readers like never before.',
-    about: 'Quill is built with <i class="heart icon"></i><em>love</em> by <a href="https://maki.io/people">a diverse community of makers</a> from all around the world.  We\'re designing an all-new way to publish things on the web.  <a class="ui mini primary unpadded button" href="https://maki.io/topics/quill">Learn More &raquo;</a>',
+    about: 'Quill is built with <i class="heart icon"></i><em>love</em> by <a href="https://maki.io/people">a diverse community of makers</a> from all around the world.  We\'re designing an all-new way to publish things on the web.  <a href="https://maki.io/topics/quill">Join the Conversation &raquo;</a>',
     copyright: '&copy; 2017 Quill, Inc.</small>',
     masthead: 'images/typewriter.jpg',
     cta: {
